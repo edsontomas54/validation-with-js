a240aa938f471bd3274cccf376f5fe73fffc742e
edsontomas54 <donthimas54@gmail.com>
2023-01-03 03:41:57 +0200

    validation

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const form = document.querySelector("form")
+const errorMessage = document.getElementById("errorMessage")
+
+form.addEventListener("submit", (e)=>{
+
+    const errors =[]
+
+    if(username.value.trim() === "")
+    {
+        errors.push("Username is required...");
+    }
+    if(password.value.trim()=== "")
+    {
+       errors.push("Password is required...")
+    }
+    if(errors.length > 0)
+    {
+        e.preventDefault();
+        errorMessage.toggleAttribute("hidden");
+        errorMessage.innerHTML= errors.join(', ');
+    }
+})
